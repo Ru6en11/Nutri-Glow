175cc909e8fc7b96a4da42eb754cda8024131a1d
Vladislav Krasnoshchek <krasnoshchek.va@dvfu.ru>
2024-06-29 09:59:33 +1000

    Remove X-Powered-By header

--- main.dart
+++ main.dart
@@ -5,5 +5,5 @@ import 'package:dart_frog/dart_frog.dart';
 
 Future<HttpServer> run(Handler handler, InternetAddress ip, int port) async {
 
-  return serve(handler, ip, port);
+  return serve(handler, ip, port, poweredByHeader: null);
 }

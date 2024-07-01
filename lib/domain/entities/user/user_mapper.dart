46d235871bf1b1f3b3ada08700488b731222144b
Vladislav Krasnoshchek <krasnoshchek.va@dvfu.ru>
2024-07-02 01:04:42 +1000

    Add user from json function in mapper

--- lib/domain/entities/user/user_mapper.dart
+++ lib/domain/entities/user/user_mapper.dart
@@ -1,13 +1,15 @@
-
 import 'package:nutri_glow/domain/entities/user/user.dart';
 
 abstract class UserMapper {
+  static User fromJson(Map<String, dynamic> json) => User(
+        id: json['id'],
+        login: json['login'],
+        password: json['password'],
+      );
 
-  static Map<String, dynamic> toJson(User user) => 
-    <String, dynamic> {
-      "id": user.id,
-      "login": user.login,
-      "password": user.password,
-    };
-
+  static Map<String, dynamic> toJson(User user) => <String, dynamic>{
+        "id": user.id,
+        "login": user.login,
+        "password": user.password,
+      };
 }

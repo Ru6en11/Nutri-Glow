d9ac4bb6a994ab8503fa415ad817a8c7d07ed2d1
Vladislav Krasnoshchek <krasnoshchek.va@dvfu.ru>
2024-07-02 01:03:47 +1000

    Add sign in with jwt in auth service

--- lib/data/service/mock_auth_service.dart
+++ lib/data/service/mock_auth_service.dart
@@ -1,10 +1,15 @@
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:nutri_glow/domain/entities/user/user.dart';
 import 'package:nutri_glow/domain/service/auth_service.dart';
 
 class AuthServiceImpl implements AuthService {
+  final String _jwtSignKey;
+
+  AuthServiceImpl({required String jwtSignKey}) : _jwtSignKey = jwtSignKey;
+
   @override
-  String login() {
-    throw UnimplementedError();
+  String signIn(User user) {
+    return _generateToken(user.id);
   }
 
   @override
@@ -14,5 +19,24 @@ class AuthServiceImpl implements AuthService {
       login: login,
       password: password,
     );
+  }
+
+  String _generateToken(String userId) {
+    final JWT jwt = JWT({'userId': userId});
+    final String secret =
+        jwt.sign(SecretKey(_jwtSignKey), expiresIn: Duration(seconds: 60));
+
+    return secret;
+  }
+
+  bool _checkTokenForValidity(String token) {
+    try {
+      final JWT? jwt = JWT.verify(token, SecretKey(_jwtSignKey));
+      if (jwt == null) return false;
+
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }

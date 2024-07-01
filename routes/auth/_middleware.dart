65309edf2279848f12ba1f5a81dc30a475860e2f
Vladislav Krasnoshchek <krasnoshchek.va@dvfu.ru>
2024-07-01 18:03:16 +1000

    Add  middleware for auth route

---
+++ routes/auth/_middleware.dart
@@ -0,0 +1,15 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:nutri_glow/data/gateway/mock_user_gateway.dart';
+import 'package:nutri_glow/data/service/mock_auth_service.dart';
+import 'package:nutri_glow/domain/entities/user/user_gateway.dart';
+import 'package:nutri_glow/domain/service/auth_service.dart';
+
+Handler middleware(Handler handler) {
+  return handler
+    .use(
+    provider<UserGateway>((_) => MockUserGateway())
+    )
+    .use(
+      provider<AuthService>((_) => AuthServiceImpl())
+    );
+}

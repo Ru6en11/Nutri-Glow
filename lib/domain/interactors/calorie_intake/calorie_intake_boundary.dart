399f1832ed7f9901b16fbf54a6ebafa055bf2aa3
Vladislav Krasnoshchek <krasnoshchek.va@dvfu.ru>
2024-07-03 09:26:05 +1000

    Remove unused import

--- lib/domain/interactors/calorie_intake/calorie_intake_boundary.dart
+++ lib/domain/interactors/calorie_intake/calorie_intake_boundary.dart
@@ -1,6 +1,4 @@
 
-import 'package:nutri_glow/domain/entities/anthropometric_data/anthropometric_data.dart';
-
 abstract class CalorieIntakeBoundary {
 
     Map<String, dynamic> execute(String userId);

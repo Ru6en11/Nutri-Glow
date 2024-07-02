eab9999ae12fd39da058b446a758ef436b835ad6
Vladislav Krasnoshchek <krasnoshchek.va@dvfu.ru>
2024-07-02 19:28:49 +1000

    Add anthropometric data entity

---
+++ lib/domain/entities/anthropometric_data/anthropometric_data.dart
@@ -0,0 +1,19 @@
+class AnthropometricData {
+  final String userId;
+  final String gender;
+  final int age;
+  final int weight;
+  final int height;
+  final String activityLevel;
+  final String goal;
+
+  const AnthropometricData({
+    required this.userId,
+    required this.gender,
+    required this.age,
+    required this.weight,
+    required this.height,
+    required this.activityLevel,
+    required this.goal,
+  });
+}
